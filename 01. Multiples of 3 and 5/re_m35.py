f9a5723f562b0873e69f6d39c224eed1bb52447b
RiasKlein <tseshunman@gmail.com>
2016-04-26 10:26:38 -0400

    Timing and Optimization for Q1
    - Timing code added
    - A second attempt at question 1 is included as well

--- 01. Multiples of 3 and 5/re_m35.py
+++ 01. Multiples of 3 and 5/re_m35.py
@@ -1,6 +1,6 @@
 #################################################################################
 #																				
-#	m35.py 																		
+#	re_m35.py 																		
 #																				
 #	Project Euler (projecteuler.net) Problem 1 									
 #	Multiples of 3 and 5 														
@@ -20,39 +20,18 @@ import time
 # Global variable
 max_value = 1000	# Value to stop at 
 
-# Append Multiples
-#	Function adds all multiples of 'number' up until 'max_value' into 'list'
-#	If a value is already in the list it is not appended again
-def append_multiples ( number, max_value, list ):
-	cur_mult = 1 	# The current multiplier for number
-	cur_val = number * cur_mult
-
-	while (cur_val < max_value):
-		if cur_val not in list:
-			list.append (cur_val)
-
-		cur_mult += 1
-		cur_val = number * cur_mult
-
-	return list
-
-# sum_list
-#	Function adds all the numbers in the list and returns the sum
-def sum_list ( list ):
-	sum = 0
-	for num in list:
-		sum += int (num)
-	return sum
-
 # main
-#	Function appends multiples of 3 & 5 into list, stopping at max_value
-#	Repeats are skipped. Then the sum is computed and the result is printed
+#	Function iterates from 
 def main ():
-	list = []
-	list = append_multiples (3, max_value, list)
-	list = append_multiples (5, max_value, list)
-	sum = sum_list (list)
+	sum = 0
 
+	# Iterate from 1 to the stopping point
+	for x in range (1, max_value):
+		# Only add the current value if it is a multiple of 3 or 5
+		if (x % 3 == 0) or (x % 5 == 0):
+			sum += x
+
+	# Print the output
 	print ("The sum of all multiples of 3 or 5 below " + str(max_value) + " is: " + str(sum))
 
 time_start = time.time()
