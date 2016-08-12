142a66b78e3296ca423716c4f8f96d9e550e9089
RiasKlein <tseshunman@gmail.com>
2016-08-12 11:54:32 -0400

    27 Complete and Functional
    - pair programming solution
    - utilizes a modified primes generator

---
+++ 027. Quadratic primes/quadratic.py
@@ -0,0 +1,67 @@
+################################################################################
+#
+#   quadratic.py
+# 
+#   Project Euler 27
+#   Quadratic Primes
+#
+#   Considering quadratics of the form:
+#       n^2 + an + b, where |a| < 1000 and |b| <= 1000
+#   where |n| is the absolute value of n.
+#
+#   Find the product of the coefficients, a and b, for the quadratic expression 
+#   that produces the maximum number of primes for consecutive values of n,
+#   starting with n = 0.
+#
+#   Program made via pair programming by Shunman Tse & David Tan
+#
+################################################################################
+
+def main():
+    longest_chain = 0
+    product_lc = 0
+    
+    # Generate our prime markers
+    markers = prime_markers (1000000)
+
+    for a in range (-999, 1000):        # a goes from [-999, 1000)
+        for b in range (2, 1001):       # b goes from [2, 1000] , optimization: b >= 2 for n = 0 case
+            current_chain = 0
+            n = 0
+            
+            if markers[b] == True:
+                while True:
+                    val = n**2 + a * n + b
+                    if val > 0 and markers[val]:
+                        current_chain += 1
+                        n += 1
+                    else:
+                        break
+                if current_chain > longest_chain:
+                    longest_chain = current_chain
+                    product_lc = a * b
+    
+    print (product_lc)
+
+# prime_markers
+# Returns a list of boolean markers for whether a number is prime or not
+# Assuming the returned list is named markers:
+# If you want to test whether 3 is prime,
+# check whether markers[3] == True
+def prime_markers ( n ):
+    markers = [False] * n       # Boolean list of markers to be returned
+    sieve = [True] * n
+    for i in xrange(3, int(n**0.5) + 1, 2):
+        if sieve[i]:
+            sieve[i * i::2 * i] = [False] * ((n - i * i - 1) / (2 * i) + 1)
+    
+    # 2 is always a prime number, mark it as such
+    markers[2] = True
+    
+    # Use the sieve to complete our prime markers
+    for i in xrange (3, n, 2):
+        if sieve[i]:
+            markers[i] = True
+    return markers
+    
+main()
